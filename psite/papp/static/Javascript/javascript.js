f404a8d455b24bfb1a909db479d337c146450598
Caleb <caleb.broughtondelong@gmail.com>
2021-02-16 23:50:44 -0500

    Successfully linked projects to tasks

--- psite/papp/static/Javascript/javascript.js
+++ psite/papp/static/Javascript/javascript.js
@@ -2,11 +2,14 @@
 function change_visibility(object)
 {
     var d = document.getElementById(object)
+
     if (d.style.visibility == 'hidden') {
-    d.style.visibility = 'visible'
+    d.style.visibility = 'visible';
+    d.style.height = '300px';
+    d.style.width = '300px';
     }
     else {
-    d.style.visibility = 'hidden'
+    d.style.visibility = 'hidden';
     }
 
 }
